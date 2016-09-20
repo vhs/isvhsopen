a97725a5dd97497ab67e6937866704c256b6e13d
Tycho (Ty) Eggen <tycho.eggen@gmail.com>
2016-09-20 16:09:56 -0700

    Updated calendar ID

--- app/calendar.jsx
+++ app/calendar.jsx
@@ -16,7 +16,7 @@ export default class EventCalendar extends React.Component {
     }
 
     componentDidMount() {
-        var calId = "3n0o5q5ldon1dj0d3gifunmmtc%40group.calendar.google.com";
+        var calId = "3n0o5q5ldon1dj0d3gifunmmtc";
         var publicKey = "AIzaSyARGs7V5MKve8XTreHbE61amTkEX05ulBQ";
         var now = moment().startOf('day');
         var maxDate = moment().add(30, 'days');
