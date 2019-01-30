7295d28cbef9ec08ffdd24af66bb8c8d4d7dbc5f
Ty Eggen <tycho.eggen@gmail.com>
2019-01-29 20:38:27 -0800

    Updated stats controller for the new InfluxDB interface

--- controller/stats.js
+++ controller/stats.js
@@ -8,72 +8,88 @@ var Influx = require('influx'),
 var _instance;
 
 function writeEvent(event) {
-    return new Promise(function(resolve, reject){
-        var points = [
-            [{ value: event.newStatus, duration: event.duration }, { space: "vhs", name: "door" }]
-        ];
-        _instance.writePoints("api", points, function(err){
-            if (err) {
-                return reject(err);
-            }
-            return resolve();
-        });
-    });
+    debug("writeEvent:", event);
+    var points = [{
+        measurement: 'api',
+        tags: {
+            name: "door",
+            space: "vhs"
+        },
+        fields: {
+            duration: event.duration,
+            value: event.newStatus
+        },
+    }];
+    debug("writing points:", points);
+    return _instance.writePoints(points);
 }
 
 function getLastStatus() {
-    return new Promise(function(resolve, reject){
-        var query = "select * from api where space='vhs' and \"name\"='door' order by time desc limit 1;";
-        _instance.query(query, function(err, results) {
-            var unknown = {
-                status: "unknown",
-                last: new Date()
-            };
-            if (err) {
-                return resolve(unknown);
-            }
+    var result_set = {
+        status: "unknown",
+        last: new Date()
+    };
+
+    debug("getLastStatus", "Getting last status");
+    var query = "select * from api where space='vhs' and \"name\"='door' order by time desc limit 1;";
+    debug("getLastStatus", "Doing query");
+    return _instance.query(query)
+        .then(function (results) {
+            debug("Getting last status query result:");
+            debug(results);
             if (results[0] && results[0][0]) {
-                var result = results[0][0];
-                return resolve({
-                    status: result.value,
-                    last: new Date(result.time)
-                });
+                debug("getLastStatus", "Got result");
+                let item = results[0][0];
+                result_set.status = item.value;
+                result_set.last = new Date(item.time);
+                return result_set;
             }
-            return resolve(unknown);
+            debug("getLastStatus", "Got unknown result");
+            return result_set;
+        })
+        .catch(function (err) {
+            debug("getLastStatus", "Got error");
+            return result_set;
         });
-    });
 }
 
-module.exports.setup = function() {
+module.exports.setup = function () {
     var options = {
         host: config.get("influxHost"),
         port: config.get("influxPort"),
         username: config.get("influxUser"),
         password: config.get("influxPw"),
-        protocol : 'http',
+        protocol: 'http',
         database: config.get("influxDb")
     };
-    _instance = Influx(options);
+    debug("setup", options);
+    _instance = new Influx.InfluxDB(options);
     var state;
-    return stateController.currentState().then(function(s) {
-        state = s;
-        state.on("change", function(event) {
-            writeEvent(event).then(function(){
-                debug("Wrote changes to influx");
-                debug(event);
-            })
-            .catch(function(err){
-                debug(err);
-            })
+    return stateController.currentState().then(function (s) {
+            debug("Getting currentState");
+            state = s;
+            debug("Registering change listener");
+            state.on("change", function (event) {
+                writeEvent(event)
+                    .then(function (data) {
+                        debug("Wrote changes to influx");
+                        debug("event:", event);
+                        debug("data:", data);
+                        return data;
+                    })
+                    .catch(function (err) {
+                        debug(err);
+                    })
+            });
+            debug("Returning getLastStatus");
+            return getLastStatus();
+        })
+        .then(function (last) {
+            state.init(last);
+        })
+        .catch(function (err) {
+            debug(err);
         });
-        return getLastStatus();
-    })
-    .then(function(last){
-        state.init(last);
-    })
-    .catch(function(err){
-        debug(err);
-    });
 };
 
 module.exports.getLastStatus = getLastStatus;
